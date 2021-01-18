9086765be5200c932b322b74753fa28cf63e4d06
kaito <kaitat1231@gmail.com>
2021-01-19 01:34:55 +0900

    add readme and setsh

---
+++ README.md
@@ -0,0 +1,5 @@
+# dotfiles
+- linux centos7 の環境でのdotfiles以下実行
+```
+$sh set.sh
+```
---
+++ set.sh
@@ -0,0 +1,5 @@
+  ln -s dotfiles/.vim ~/
+  ln -s dotfiles/.zsh ~/
+  ln -s dotfiles/.tigrc ~/
+  ln -s dotfiles/.vimrc ~/
+  ln -s dotfiles/.zshrc ~/
